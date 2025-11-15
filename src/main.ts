f01c399d2c352126827d939cb43112494a010a51
it16knitto <it16.knitto@gmail.com>
2025-11-15 10:20:45 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
+import { ResponseInterceptor } from './interceptors/response-interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './filter/exception-filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  // Access the version field
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs')
+    .setDescription(`Nestjs - Backend`)
+    .setVersion('1.0')
+    .addServer(
+      env.SWAGGER_BASEPATH
+        ? env.SWAGGER_BASEPATH
+        : 'http://localhost:' + (env.APP_PORT ?? 3000),
+    )
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app as any, config);
+  SwaggerModule.setup('swagger', app as any, document);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  await app.listen(env.APP_PORT ?? 3000);
 }
 bootstrap();
